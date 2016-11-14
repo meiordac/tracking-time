84ab3e0112f74e3212bc7a2e6bef0684efa34548
Matias Iordache <matias@acid.cl>
2016-11-14 13:34:51 -0300

    create customer class

---
+++ TrackingTime/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrackingTime.Models
+{
+    public class Customer
+    {
+        public string Name { get; }
+        public string Contact { get; }
+        public string Phone { get; }
+        public string Status { get; }
+
+        public Customer(string name, string contact, string phone, string status)
+        {
+            Name = name;
+            Contact = contact;
+            this.Phone = phone;
+            this.Status = status;
+        }
+    }
+}
